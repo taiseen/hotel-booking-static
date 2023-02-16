2d3bf333d964ac7aaa81490b531f24ee52038572
taiseen <taiseen.cse@gmail.com>
2023-02-16 16:45:57 +0600

    fix: data pass by context api

--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -1,21 +1,14 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
+import { roomData } from "../db/data";
 
 const RoomInfo = createContext();
 
 export const RoomContext = ({ children }) => {
 
-  const [price, setPrice] = useState(1500);
-
-  // return all properties...
-  // useEffect(() => {
-  //   const allProperty = houses.map(house => house.type);
-  //   const uniqueProperties = ['Property (any)', ...new Set(allProperty)];
-  //   setProperties(uniqueProperties);
-  // }, []);
+  const [rooms, setRooms] = useState(roomData);
 
   const shareWithChildren = {
-    price,
-    setPrice,
+    rooms,
   };
 
 
