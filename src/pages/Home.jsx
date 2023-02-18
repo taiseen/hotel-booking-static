bd8f06516b83df788543dd248566175e5f1b1931
taiseen <taiseen.cse@gmail.com>
2023-02-18 16:39:20 +0600

    fix: Room component add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Room } from '../components';
 import { LogoDark } from '../assets';
+
 
 const Home = () => {
   return (
     <div className='text-2xl text-red-400'>
       Home
       <LogoDark />
+      <Room />
     </div>
   );
 };
