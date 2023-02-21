a2cd1bbbd365e74856d02e42f31535ebfcb0814f
taiseen <taiseen.cse@gmail.com>
2023-02-21 23:42:46 +0600

    fix: footer date add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <footer className='bg-black/80 py-8 text-center text-white'>
+    <div className='container mx-auto'>Copyright &copy; {new Date().getFullYear()}, All Right Reserved, Taiseen</div>
+  </footer>
+);
 
 export default Footer;
