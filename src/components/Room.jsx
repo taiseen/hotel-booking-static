0eb3ad01681f983030eb8575d99d9276f29c99ae
taiseen <taiseen.cse@gmail.com>
2023-02-23 22:30:10 +0600

    fix: rooms img add

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Room = () => {
-  return <div>Room</div>;
+const Room = ({ room }) => {
+
+  const { id, name, image, size, maxPerson, description, price } = room;
+
+  return (
+    <div className='bg-white shadow-2xl min-h-[500px] group'>
+
+      <div className='overflow-hidden'>
+        <img src={image} alt="img" className='group-hover:scale-110 transition-all duration-300 w-full' />
+      </div>
+
+      <div className='bg-white shadow-lg max-w-[300px] mx-auto h-[60px] -translate-y-1/2 flex justify-center items-center'>
+        Details
+      </div>
+    </div>
+  );
+
 };
 
 export default Room;
--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -11,11 +11,13 @@ const Rooms = () => {
 
       <div className='container mx-auto lg:px-0'>
 
-        {
-          rooms.map(room =>
-            <Room key={room.id} room={room} />
-          )
-        }
+        <div className='grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0'>
+          {
+            rooms.map(room =>
+              <Room key={room.id} room={room} />
+            )
+          }
+        </div>
       </div>
 
     </section>
