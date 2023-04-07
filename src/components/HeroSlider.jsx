859310b3d23093af2652e617bfadea8dd6c107b9
taiseen <taiseen.cse@gmail.com>
2023-04-07 23:32:41 +0600

    feat: add swiper slider in hero section

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { sliderData } from '../constants/data';
+import { EffectFade, Autoplay } from 'swiper';
+import 'swiper/css/effect-fade';
+import 'swiper/css';
+
 
 const HeroSlider = () => {
+
   return (
-    <div className='heroSlider h-[600px] lg:h-[860px] bg-black'>
-      
-    </div>
+    <Swiper className='heroSlider h-[600px] lg:h-[860px] bg-black'>
+      {
+        sliderData.map(({ id, title, bg, btnNext }) =>
+          <SwiperSlide className='h-full'>
+            <div>
+              <div>Just Enjoy & Relax</div>
+            </div>
+            <div className='absolute top-0 w-full h-full'>
+              <img className='object-cover h-full w-full' src={bg} alt="logo" />
+            </div>
+          </SwiperSlide>
+        )
+      }
+    </Swiper>
   )
 };
 
--- src/constants/data.js
+++ src/constants/data.js
@@ -1,3 +1,6 @@
+import images from "../assets";
+
+
 export const adultsList = [
     { name: '1 Adult' },
     { name: '2 Adults' },
@@ -5,10 +8,34 @@ export const adultsList = [
     { name: '4 Adults' },
 ]
 
+
 export const kidsList = [
     { name: '0 Kid' },
     { name: '1 Kid' },
     { name: '2 Kids' },
     { name: '3 Kids' },
     { name: '4 Kids' },
+]
+
+
+export const sliderData = [
+    {
+        id: 1,
+        title: 'Your Luxury Hotel For Vacation',
+        bg: images.Slider1,
+        btnNext: 'Room & Suites',
+    },
+    {
+        id: 2,
+        title: 'Your Luxury Hotel For Vacation',
+        bg: images.Slider2,
+        btnNext: 'Room & Suites',
+    },
+    {
+        id: 3,
+        title: 'Your Luxury Hotel For Vacation',
+        bg: images.Slider3,
+        btnNext: 'Room & Suites',
+    },
+
 ]
